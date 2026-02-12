3e95e3b934dc8ef0d99728ae07c3964c162facad
Eleazar <dievexx@gmail.com>
2026-02-12 21:54:22 +0100

    cambios menores para plotear la leyenda fuera de la figura

--- k_brazos/src/plotting/plotting.py
+++ k_brazos/src/plotting/plotting.py
@@ -43,10 +43,7 @@ def get_algorithm_label(algo) -> str:
         # Aquí añadimos la lógica para tu nuevo algoritmo
         label += f" (tau={algo.tau})"
 
-    
-    
     else:
-
         raise ValueError("El algoritmo debe ser de la clase Algorithm o una subclase.")
     
         
@@ -57,7 +54,6 @@ def plot_average_rewards(steps: int, rewards: np.ndarray, algorithms: List[Algor
     Genera la gráfica de Recompensa Promedio vs Pasos de Tiempo.
     """
     sns.set_theme(style="whitegrid", palette="muted", font_scale=1.2)
-
     plt.figure(figsize=(14, 7))
     for idx, algo in enumerate(algorithms):
         label = get_algorithm_label(algo)
@@ -66,14 +62,14 @@ def plot_average_rewards(steps: int, rewards: np.ndarray, algorithms: List[Algor
     plt.xlabel('Pasos de Tiempo', fontsize=14)
     plt.ylabel('Recompensa Promedio', fontsize=14)
     plt.title('Recompensa Promedio vs Pasos de Tiempo', fontsize=16)
-    plt.legend(title='Algoritmos')
+    plt.legend(title='Algoritmos', bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
     plt.show()
 
 def plot_optimal_selections(steps: int, optimal_selections: np.ndarray, algorithms: List[Algorithm]):
     """
+    Función OBLIGATORIA.
     Genera la gráfica de Porcentaje de Selección del Brazo Óptimo vs Pasos de Tiempo.
-
     :param steps: Número de pasos de tiempo.
     :param optimal_selections: Matriz de porcentaje de selecciones óptimas.
     :param algorithms: Lista de instancias de algoritmos comparados.
@@ -87,7 +83,7 @@ def plot_optimal_selections(steps: int, optimal_selections: np.ndarray, algorith
     plt.xlabel('Pasos de Tiempo', fontsize=14)
     plt.ylabel('% Selección Óptima', fontsize=14)
     plt.title('Porcentaje de Selección del Brazo Óptimo vs Pasos de Tiempo', fontsize=16)
-    plt.legend(title='Algoritmos')
+    plt.legend(title='Algoritmos', bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.ylim([0, 100])
     plt.tight_layout()
     plt.show()
@@ -95,9 +91,8 @@ def plot_optimal_selections(steps: int, optimal_selections: np.ndarray, algorith
 
 def plot_arm_statistics(arm_stats: List[dict], algorithms: List[Algorithm], *args):
     """
-    Mostrar la evolución del rechazo, de forma análoga a cómo se muestra la evolución de las recompensas.
-    Genera gráficas separadas de Selección de Arms:
-    Ganancias vs Pérdidas para cada algoritmo.
+    Función OBLIGATORIA.
+    Genera gráficas separadas de Selección de Arms: Ganancias vs Pérdidas para cada algoritmo.
     :param arm_stats: Lista (de diccionarios) con estadísticas de cada brazo por algoritmo.
     :param algorithms: Lista de instancias de algoritmos comparados.
     :param args: Opcional. Parámetros que consideres
@@ -107,11 +102,11 @@ def plot_arm_statistics(arm_stats: List[dict], algorithms: List[Algorithm], *arg
     for idx, algo in enumerate(algorithms):
         label = get_algorithm_label(algo)
         plt.plot(range(len(arm_stats[idx]["wins"])), arm_stats[idx]["wins"], label=f"{label} - Wins", linewidth=2)
-        plt.plot(range(len(arm_stats[idx]["losses"])), arm_stats[idx]["losses"], label=f"{label} - Losses", linewidth=2)
+        #plt.plot(range(len(arm_stats[idx]["losses"])), arm_stats[idx]["losses"], label=f"{label} - Losses", linewidth=2)  # Losses = 1 - wins
     plt.xlabel('Pasos de Tiempo', fontsize=14)
     plt.ylabel('Selección de Brazos', fontsize=14)
     plt.title('Selección de Brazos vs Pasos de Tiempo', fontsize=16)
-    plt.legend(title='Algoritmos')
+    plt.legend(title='Algoritmos', bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
     plt.show()
 
@@ -133,7 +128,7 @@ def plot_regret(steps: int, regret_accumulated: np.ndarray, algorithms: List[Alg
     plt.xlabel('Pasos de Tiempo', fontsize=14)
     plt.ylabel('Regret Acumulado', fontsize=14)
     plt.title('Regret Acumulado vs Pasos de Tiempo', fontsize=16)
-    plt.legend(title='Algoritmos')
+    plt.legend(title='Algoritmos', bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
     plt.show()
 
