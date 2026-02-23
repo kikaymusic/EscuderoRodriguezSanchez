235421c67ba089aedce9974a294eea421d82aea9
Jesús Sánchez Ochoa <jesussanchezochoa19@gmail.com>
2026-02-23 18:48:33 +0100

    feat: añadida clase train para entrenar agentes. Actualizado notebook_tabular

--- Entornos_Complejos/src/utils.py
+++ Entornos_Complejos/src/utils.py
@@ -2,34 +2,6 @@ import numpy as np
 
 SEMILLA = 42
 
-def ensure_state_in_q(agent, state):
-    """
-    Garantiza que un estado exista en las tablas del agente Monte Carlo antes de usarlo.
-
-    Esta función inicializa, si el estado no existe todavía:
-    - agent.q_table[state]: estimaciones de valor de acción Q(s,a)
-    - agent.c_table[state]: acumuladores/pesos (o recuentos) usados en las
-      actualizaciones Monte Carlo off-policy
-
-
-    La política de comportamiento epsilon-greedy puede intentar consultar Q(s,a)
-    para un estado que aún no ha aparecido en la tabla. Como el agente Monte Carlo
-    actualiza los valores al final del episodio, puede ocurrir que un estado se use
-    durante la selección de acción antes de haber sido inicializado. Esta función evita
-    errores (por ejemplo, KeyError) y garantiza una entrada válida inicializada a cero.
-
-    Parámetros
-    agent : AgentMonteCarlo
-        Instancia del agente Monte Carlo que contiene q_table y c_table.
-    state : tuple
-        Estado de Blackjack con la forma:
-        (suma_jugador, carta_visible_dealer, usable_ace).
-    """
-    if state not in agent.q_table:
-        n_actions = agent.env.action_space.n
-        agent.q_table[state] = np.zeros(n_actions, dtype=np.float32)
-        agent.c_table[state] = np.zeros(n_actions, dtype=np.float32)
-
 
 def q_to_v_and_policy(Q):
     """
