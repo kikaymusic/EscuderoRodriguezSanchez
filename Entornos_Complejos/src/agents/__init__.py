44c67c4110a9a3e9b240c9769dabcd936580f11e
Eleazar <dievexx@gmail.com>
2026-02-25 11:17:45 +0100

    feat: añadidos agentes y politicas a su respectivo __init__

---
+++ Entornos_Complejos/src/agents/__init__.py
@@ -0,0 +1,13 @@
+from .agent import Agent
+from .sarsa import AgentSarsa
+from .sarsa_semigradient import AgentSarsaSemiGradient
+from .qlearning import AgentQLearning
+from .montecarlo import AgentMonteCarlo
+
+__all__ = [
+    'Agent',
+    'AgentSarsa',
+    #'AgentSarsaSemiGradient',
+    'AgentQLearning',
+    'AgentMonteCarlo'
+]
---
+++ Entornos_Complejos/src/policies/__init__.py
@@ -0,0 +1,9 @@
+from .policy import Policy
+from .epsilon_greedy import EpsilonGreedyPolicy
+from .epsilon_soft import EpsilonSoftPolicy
+
+__all__ = [
+    'Policy',
+    'EpsilonGreedyPolicy',
+    'EpsilonSoftPolicy'
+]
