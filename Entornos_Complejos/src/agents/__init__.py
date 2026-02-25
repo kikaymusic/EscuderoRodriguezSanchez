32060353602c477039c1467998e620b41362e97e
Eleazar <dievexx@gmail.com>
2026-02-25 20:35:55 +0100

    fix: arreglado import de sarsa que esta en la otra rama

--- Entornos_Complejos/src/agents/__init__.py
+++ Entornos_Complejos/src/agents/__init__.py
@@ -1,13 +1,11 @@
 from .agent import Agent
 from .sarsa import AgentSarsa
-from .sarsa_semigradient import AgentSarsaSemiGradient
 from .qlearning import AgentQLearning
 from .montecarlo import AgentMonteCarlo
 
 __all__ = [
     'Agent',
     'AgentSarsa',
-    #'AgentSarsaSemiGradient',
     'AgentQLearning',
     'AgentMonteCarlo'
 ]
